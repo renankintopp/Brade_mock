882449ff4f218a13f22d9d4f80c8d7b6af058f09
Henrique Lima <henri.limas@gmail.com>
2026-09-11 16:05:26 -0300

    Update cdh-local.js

--- Banco Bradesco _ Você primeiro_files/cdh-local.js
+++ Banco Bradesco _ Você primeiro_files/cdh-local.js
@@ -1,4 +1,16 @@
 (function () {
+  function updateLearnMoreLabels() {
+    var labels = document.querySelectorAll('.cdh-extension-wrap .cdh-learnmore-label');
+    labels.forEach(function (label) {
+      label.textContent = 'Saiba mais';
+    });
+
+    var rejectButtons = document.querySelectorAll('.cdh-extension-wrap .reject');
+    rejectButtons.forEach(function (button) {
+      button.textContent = 'Ver depois';
+    });
+  }
+
   function closeCard(card) {
     var details = card.querySelector('.hidden-details');
     var trigger = card.querySelector('.learnmore-cdh');
@@ -7,11 +19,26 @@
   }
 
   document.addEventListener('click', function (event) {
-    var trigger = event.target.closest('.cdh-extension-wrap .learnmore-cdh');
     var accept = event.target.closest('.cdh-extension-wrap .accept');
     var reject = event.target.closest('.cdh-extension-wrap .reject');
+    var trigger = event.target.closest('.cdh-extension-wrap .learnmore-cdh');
+
+    if (reject) {
+      event.preventDefault();
+      event.stopPropagation();
+      var card = reject.closest('.front-option, .tile');
+      if (card) card.hidden = true;
+      return;
+    }
+
+    if (accept) {
+      closeCard(accept.closest('.front-option, .tile'));
+      return;
+    }
 
     if (trigger) {
+      event.preventDefault();
+      event.stopPropagation();
       var card = trigger.closest('.front-option, .tile');
       var details = card && card.querySelector('.hidden-details');
       if (!details) return;
@@ -19,9 +46,7 @@
       trigger.setAttribute('aria-expanded', String(visible));
       return;
     }
-
-    if (accept || reject) {
-      closeCard((accept || reject).closest('.front-option, .tile'));
-    }
   });
+
+  updateLearnMoreLabels();
 })();
